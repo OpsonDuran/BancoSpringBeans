1a9721ad2fce198d788a902500bf0b1456142fe5
Armando Duran Salvador <iccarmandoduran@gmail.com>
2017-07-06 14:34:38 -0500

    projecto inicial

---
+++ src/main/java/com/beeva/spring/BancoSpringBeans/App.java
@@ -0,0 +1,13 @@
+package com.beeva.spring.BancoSpringBeans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
